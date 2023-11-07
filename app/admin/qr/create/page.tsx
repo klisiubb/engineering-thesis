24bff3e9f5bc6c184701e2dbb22d3bf8582042d7
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2023-11-07 15:36:51 +0100

    schema changes

---
+++ app/admin/qr/create/page.tsx
@@ -0,0 +1,111 @@
+"use client";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { z } from "zod";
+import Link from "next/link";
+import toast from "react-hot-toast";
+import { redirect, useRouter } from "next/navigation";
+import { useUser } from "@clerk/nextjs";
+import { Role } from "@prisma/client";
+import {
+  Form,
+  FormField,
+  FormControl,
+  FormLabel,
+  FormDescription,
+  FormMessage,
+  FormItem,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: "Name is required",
+  }),
+});
+
+const CreatePage = () => {
+  const { isLoaded, isSignedIn, user } = useUser();
+  const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const response = await axios.post("/api/admin/qr/create", values);
+      router.push(`/admin/qr/edit/${response.data.id}`);
+      toast.success("QR Code created");
+    } catch (error) {
+      console.error(error);
+      toast.error("QR Code with this name already exists!");
+    }
+  };
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isSignedIn || user.publicMetadata.role !== Role.ADMIN) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="h-full grid place-items-center">
+      <div className="max-w-5xl w-full mx-auto p-6">
+        <h1 className="text-2xl">QR Code</h1>
+        <p className="text-sm text-slate-600">
+          What is the name of the QR Code? Don&apos;t worry, you can change this
+          later.
+        </p>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 mt-8"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>QR Code name:</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="e.g. 'React workshop QR Code'"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    How will you name your QR Code?
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="grid grid-cols-2 gap-2 items-center justify-center">
+              <Link href="/admin/qr/">
+                <Button type="button" variant="ghost">
+                  Cancel
+                </Button>
+              </Link>
+              <Button type="submit" disabled={!isValid || isSubmitting}>
+                Continue
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePage;
